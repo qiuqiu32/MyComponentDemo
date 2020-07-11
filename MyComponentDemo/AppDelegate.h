15774d6607c7b13b8e72399c39c49a67312e94bd
“qiuqiu32” <1442687881@qq.com>
2020-07-11 16:26:54 +0800

    Initial Commit

---
+++ MyComponentDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyComponentDemo
+//
+//  Created by mac on 2020/7/11.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
